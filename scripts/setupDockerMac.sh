a1bef5154d1e936d48c76ef76c97bbe116d57d8d
falenn <dev@imwiz.com>
2016-11-11 10:57:41 -0500

    install docker elements on mac

---
+++ scripts/setupDockerMac.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Update cask first
+brew cask update
+
+# See what to install
+brew search docker
+
+# Install Docker
+brew install docker
+brew install docker-clean
+brew install docker-tools
+# Install Docker compose and docker machine
+brew install docker-compose
+
+# brew items may already be installed.  The above
+# commands will fail-fast.  Now, update
+brew upgrade docker*
+
+# show what's installed
+brew list
